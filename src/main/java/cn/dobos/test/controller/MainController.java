54af3ed372b69d74cf39c86948b6f32bf4fd66c9
洪志胜 <hongzhisheng@souche.com>
2019-11-25 16:55:57 +0800

    test jenkins
    Change-Id: I91a86c518a09f8bebe2a093d337a66270627e756

--- src/main/java/cn/dobos/test/controller/MainController.java
+++ src/main/java/cn/dobos/test/controller/MainController.java
@@ -7,4 +7,14 @@ package cn.dobos.test.controller;
  * Created on 2019-11-25
  */
 public class MainController {
+
+    /**
+     * 首页
+     *
+     * @return
+     */
+    public String index() {
+        return "index";
+    }
+
 }
