18c3176a9e907baca293c63cfbc9d8977fb847cf
洪志胜 <hongzhisheng@souche.com>
2019-11-25 09:27:09 +0800

    修改了Application类
    Change-Id: Ia9c6b895f9d643ae372a7d05c82a41d192b59ad2

--- src/main/java/cn/dobos/test/Application.java
+++ src/main/java/cn/dobos/test/Application.java
@@ -4,6 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
+
+    }
+
+
+    public void test() {
+        System.out.println("test method");
     }
 
 }
